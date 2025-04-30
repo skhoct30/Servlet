f9d26864b18be0b2627bb5265f25c24441b255d0
skhoct30 <skhoct30@naver.com>
2025-04-30 18:45:21 +0900

    [서강현] - servlet/Ex05 수정

--- src/main/java/com/marondal/servlet/ex/Ex05Controller.java
+++ src/main/java/com/marondal/servlet/ex/Ex05Controller.java
@@ -6,8 +6,18 @@ import javax.servlet.http.HttpServletResponse;
 
 public class Ex05Controller extends HttpServlet {
 	
+	// 파라미터가
+	// 보안에 민감한 데이터가 포험되어 있는 지
+	// 긴 데이터가 포험되어 있는 지
+	// doPost
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) {
+		
+		//이름과 생년월일을 전달받고,
+		// 이름과 나이를 html로 구성
+		
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html");
 		
 		
 		
