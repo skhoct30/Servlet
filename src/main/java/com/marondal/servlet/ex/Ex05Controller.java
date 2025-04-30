4e03ac69967f2bc8c3ec57dde84e9600a2fb2962
skhoct30 <skhoct30@naver.com>
2025-04-30 18:29:27 +0900

    [서강현] - 새로운 서플릿 추가

---
+++ src/main/java/com/marondal/servlet/ex/Ex05Controller.java
@@ -0,0 +1,17 @@
+package com.marondal.servlet.ex;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Ex05Controller extends HttpServlet {
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		
+		
+		
+		
+	}
+
+}
